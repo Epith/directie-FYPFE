ee33f1fab2b2771a8b10edbc9744f959b5df9e85
Epith <joshuachia99@yahoo.com.sg>
2018-04-26 15:54:01 +0800

    Beacon relation

--- Directie/src/pages/beacon/beacon.ts
+++ Directie/src/pages/beacon/beacon.ts
@@ -17,12 +17,56 @@ import { IBeacon } from '@ionic-native/ibeacon';
 export class BeaconPage {
   beaconRegion: any;
   beaconUUID = '11111111-1111-1111-1111-111111111111';
+  beaconRelation: any;
+  pBeaconAccuracy: any;
+  nBreaconId: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private ibeacon:IBeacon) {
-    
+    this.beaconRelation={
+      151:[{
+        PB:150,NB:152,DIR:'straight'
+      },
+      {
+        PB:149,NB:152,DIR:'turn right'
+      }],
+      152:[{
+        PB:151,NB:153,DIR:'straight'
+      }],
+      153:[{
+        PB:152,NB:154,DIR:"turn left"
+      }]
+    }
+        // Request permission to use location on iOS
+this.ibeacon.requestAlwaysAuthorization();
+// create a new delegate and register it with the native layer
+let delegate = this.ibeacon.Delegate();
+
+// Subscribe to some of the delegate's event handlers
+delegate.didRangeBeaconsInRegion()
+  .subscribe(
+    data => {
+      //console.log('didRangeBeaconsInRegion: ', data.beacons[0]['accuracy'])
+      if (data.beacons.length > 0) {
+
+        console.log(data.beacons.length);
+        for(let i=0;i<data.beacons.length;i++){
+          this.pBeaconAccuracy=data.beacons[i]["accuracy"]
+          if(this.pBeaconAccuracy<data.beacons[i]["accuracy"]){
+            this.pBeaconAccuracy=data.beacons[i]["accuracy"];
+            this.nBreaconId=data.beacons[i]["major"];
+          }
+        }
+      }
+      console.log("Nearest beacon: " + this.pBeaconAccuracy + "beaconId: "+ this.nBreaconId);
+  },
+    error => console.error()
+  );
+    this.beaconRegion = this.ibeacon.BeaconRegion('estimoteBeacon','11111111-1111-1111-1111-111111111111');
+  this.ibeacon.startRangingBeaconsInRegion(this.beaconRegion);
+  
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad BeaconPage');
+    console.dir(this.beaconRelation[151][0]["DIR"]);
   }
   
   detectBeacon(){
@@ -49,9 +93,6 @@ delegate.didEnterRegion()
     }
   );
 
-
- 
-
 this.beaconRegion = this.ibeacon.BeaconRegion('estimoteBeacon','11111111-1111-1111-1111-111111111111');
 
 this.ibeacon.startMonitoringForRegion(this.beaconRegion)
@@ -60,7 +101,6 @@ this.ibeacon.startMonitoringForRegion(this.beaconRegion)
     error => console.error('Native layer failed to begin monitoring: ', error)
   );
   this.ibeacon.startRangingBeaconsInRegion(this.beaconRegion);
-  console.log(this.ibeacon.getMonitoredRegions());
   }
   
   onBluetooth(){
