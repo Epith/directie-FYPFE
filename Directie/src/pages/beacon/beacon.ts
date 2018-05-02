cde74f96069a4931c6a60dc57c5f96380286b0a8
Epith <joshuachia99@yahoo.com.sg>
2018-05-02 12:31:38 +0800

    add determine route

--- Directie/src/pages/beacon/beacon.ts
+++ Directie/src/pages/beacon/beacon.ts
@@ -30,6 +30,7 @@ export class BeaconPage {
   beaconDetails: any;
   sub:Subscription;
   relationTest:{};
+  isFirstScan:boolean=true;
   constructor(public navCtrl: NavController, public navParams: NavParams, private ibeacon:IBeacon) {
     this.beaconRelation={
       "Beacons":[
@@ -41,7 +42,7 @@ export class BeaconPage {
             'NB':146,
             'DIR':'go straight'
           },
-        'relatedBeacons':[139,146]
+        'relatedBeacons':[139,140,146]
       },
       {
         'beaconID':146,
@@ -50,7 +51,7 @@ export class BeaconPage {
           'NB':158,
           'DIR':'go straight'
         },
-        'relatedBeacons':[140,158]
+        'relatedBeacons':[140,146,158]
       },
       {
         'beaconID':158,
@@ -64,12 +65,13 @@ export class BeaconPage {
           'NB':156,
           "DIR":'turn left'
         }],
-        'relatedBeacons':[146,153,156]
+        'relatedBeacons':[146,153,158,156]
       }
     ]
   }
-    this.sub=Observable.interval(15000).subscribe((val)=>{this.determineCurrentBeacon()})
-    //this.determineCurrentBeacon();
+    //this.determineFirstBeacon();
+    //this.sub=Observable.interval(15000).subscribe((val)=>{this.determineCurrentBeacon()})
+    
     this.beaconDetails=this.beaconRelation["Beacons"];
     this.inputDijkstra();
   }
@@ -113,81 +115,133 @@ this.ibeacon.startMonitoringForRegion(this.beaconRegion)
   }
   
   determineCurrentBeacon(){
-    // Request permission to use location on iOS
-this.ibeacon.requestAlwaysAuthorization();
-// create a new delegate and register it with the native layer
-let delegate = this.ibeacon.Delegate();
+        // Request permission to use location on iOS
+    this.ibeacon.requestAlwaysAuthorization();
+    // create a new delegate and register it with the native layer
+    let delegate = this.ibeacon.Delegate();
 
-// Subscribe to some of the delegate's event handlers
-delegate.didRangeBeaconsInRegion()
-  .subscribe(
-    data => {
-      //console.log('didRangeBeaconsInRegion: ', data.beacons[0]['accuracy'])
-      if (data.beacons.length > 0) {
-        for(let i=0;i<data.beacons.length;i++){
-          if(i==0){
-            this.pBeaconAccuracy=data.beacons[i]["accuracy"]
-            this.currentBeacon=data.beacons[i]["major"];
-          }
-          else{
-            if(this.pBeaconAccuracy>data.beacons[i]["accuracy"]){
-              this.pBeaconAccuracy=data.beacons[i]["accuracy"];
-              this.currentBeacon=data.beacons[i]["major"];
+    // Subscribe to some of the delegate's event handlers
+    delegate.didRangeBeaconsInRegion()
+      .subscribe(
+        data => {
+          //console.log('didRangeBeaconsInRegion: ', data.beacons[0]['accuracy'])
+          if (data.beacons.length > 0) {
+            for(let i=0;i<data.beacons.length;i++){
+              if(i==0){
+                this.pBeaconAccuracy=data.beacons[i]["accuracy"]
+                this.currentBeacon=data.beacons[i]["major"];
+              }
+              else{
+                if(this.pBeaconAccuracy>data.beacons[i]["accuracy"]){
+                  this.pBeaconAccuracy=data.beacons[i]["accuracy"];
+                  this.currentBeacon=data.beacons[i]["major"];
+                }
+              }
             }
           }
-        }
-      }
-  },
-    error => console.error()
-  );
-    this.beaconRegion = this.ibeacon.BeaconRegion('estimoteBeacon','11111111-1111-1111-1111-111111111111');
-  this.ibeacon.startRangingBeaconsInRegion(this.beaconRegion);
-  setTimeout(() => {
-    this.stopDetectBeacon();
-    console.log("Nearest beacon: " + this.pBeaconAccuracy + "beaconId: "+ this.currentBeacon);
-  }, 2500);
+      },
+        error => console.error()
+      );
+        this.beaconRegion = this.ibeacon.BeaconRegion('estimoteBeacon','11111111-1111-1111-1111-111111111111');
+      this.ibeacon.startRangingBeaconsInRegion(this.beaconRegion);
+      setTimeout(() => {
+        this.stopDetectBeacon();
+        //console.log("Nearest beacon: " + this.pBeaconAccuracy + "beaconId: "+ this.currentBeacon);
+        console.log("subsequent beacons");
+      }, 11000);
 
   }
+
+  determineFirstBeacon(){
+            // Request permission to use location on iOS
+        this.ibeacon.requestAlwaysAuthorization();
+        // create a new delegate and register it with the native layer
+        let delegate = this.ibeacon.Delegate();
+
+        // Subscribe to some of the delegate's event handlers
+        delegate.didRangeBeaconsInRegion()
+          .subscribe(
+            data => {
+              //console.log('didRangeBeaconsInRegion: ', data.beacons[0]['accuracy'])
+              if (data.beacons.length > 0) {
+                for(let i=0;i<data.beacons.length;i++){
+                  if(i==0){
+                    this.pBeaconAccuracy=data.beacons[i]["accuracy"]
+                    this.currentBeacon=data.beacons[i]["major"];
+                  }
+                  else{
+                    if(this.pBeaconAccuracy>data.beacons[i]["accuracy"]){
+                      this.pBeaconAccuracy=data.beacons[i]["accuracy"];
+                      this.currentBeacon=data.beacons[i]["major"];
+                    }
+                  }
+                }
+              }
+          },
+            error => console.error()
+          );
+            this.beaconRegion = this.ibeacon.BeaconRegion('estimoteBeacon','11111111-1111-1111-1111-111111111111');
+          this.ibeacon.startRangingBeaconsInRegion(this.beaconRegion);
+          setTimeout(() => {
+            this.stopDetectBeacon();
+            console.log("Nearest beacon: " + this.pBeaconAccuracy + "beaconId: "+ this.currentBeacon);
+          }, 11000);
+
+}
+
   stopDetectBeacon(){
     this.ibeacon.stopRangingBeaconsInRegion(this.beaconRegion);
   }
   
   inputDijkstra(){
    
-    /*
-     const Graph = require('node-dijkstra');
- 
-    const route = new Graph();
-    route.addNode('A', { B:1 });
-    route.addNode('B', { A:1, C:2, D: 4 });
-    route.addNode('C', { B:2, D:1 });
-    route.addNode('D', { C:1, B:4 });
     
-    this.shortestPath=route.path('A', 'D');
+     const Graph = require('node-dijkstra');
+     const route = new Graph();
+    //let obj={ B:1 };
+   /* route.addNode('139', {140:1});
+    route.addNode('140', {139:1,146:1});
+    route.addNode('146', { 140:1, 158:1});
+    route.addNode('158', { 146:1});
+    //route.addNode('D', { C:1, B:4 });
+    
+    this.shortestPath=route.path('140', '158');
 
-    console.log(this.shortestPath[0]);
+    console.log(this.shortestPath);
+    
     */
-   /*for(let i=0;i<this.beaconDetails.length;i++){
+    /*
+    */
+   for(let i=0;i<this.beaconDetails.length;i++){
     this.relatedBeacon={};
-     if(this.beaconDetails[i]["relatedBeacons"].length>1){
+     if(this.beaconDetails[i]["relatedBeacons"].length>=1){
        for(let j=0;j<this.beaconDetails[i]["relatedBeacons"].length;j++){
          if(j==0){
-          this.relatedBeacon=this.beaconDetails[i]["relatedBeacons"][j]+":1";
+           this.relatedBeacon[this.beaconDetails[i]["relatedBeacons"][j]]=1;
+          //this.relatedBeacon="{"+this.beaconDetails[i]["relatedBeacons"][j]+":1";
          }
          else{
-           this.relatedBeacon=this.relatedBeacon+", "+this.beaconDetails[i]["relatedBeacons"][j]+":1";
+          this.relatedBeacon[this.beaconDetails[i]["relatedBeacons"][j]]=1;
+           //this.relatedBeacon=this.relatedBeacon+", "+this.beaconDetails[i]["relatedBeacons"][j]+":1";
          }
        }
      }
-     this.relationTest=JSON.stringify(this.relatedBeacon);
-     console.log("beaconID:"+this.beaconDetails[i]["beaconID"]+"related: "+this.relatedBeacon);
-     //route.addNode(this.beaconDetails[i]["beaconID"],this.relatedBeacon);
-     console.log(this.relationTest);
+     route.addNode(JSON.stringify(this.beaconDetails[i]["beaconID"]),this.relatedBeacon);
+     let obj2=this.relatedBeacon;
+     //let obj3={151:1,152:1};
+     //console.log(obj);
+     //console.log(obj3);
+     console.dir("beaconID:"+this.beaconDetails[i]["beaconID"]+"related: "+(this.relatedBeacon));
+     console.log(obj2);
+    
+     //console.log(this.relationTest);
      //console.log(this.beaconDetails[i]["relatedBeacons"][0]);
+     //console.log(this.beaconDetails[i]["beaconID"]+","+obj2)
    }
-   this.shortestPath=route.path(140, 153);
-     console.log(this.shortestPath)*/
-     const graph = new Map()
+   
+  this.shortestPath=route.path(140, 153);
+    console.log(route.path('140', '156'));
+    /* const graph = new Map()
  
 const a = new Map()
 a.set('B', 1)
@@ -201,6 +255,7 @@ graph.set('A', a)
 graph.set('B', b);
  
 const route = new Graph(graph)
+ */
   }
-  
+ 
 }
