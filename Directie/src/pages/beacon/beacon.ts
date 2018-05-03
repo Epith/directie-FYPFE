d7205f2235970a1f50441274239d71b01f16f36a
Epith <joshuachia99@yahoo.com.sg>
2018-05-03 11:45:39 +0800

    Beacon detecting
    determine the beacon with relation to the previous beacon despite detecting more than 1 beacon

--- Directie/src/pages/beacon/beacon.ts
+++ Directie/src/pages/beacon/beacon.ts
@@ -38,6 +38,7 @@ export class BeaconPage {
   isFirstScan:boolean=true;
   previousBeacon:any;
   nextBeaconToGo:any;
+  testForRelated:any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private ibeacon:IBeacon, public alertCtrl:AlertController) {
     this.beaconRelation={
       "Beacons":[
@@ -49,7 +50,7 @@ export class BeaconPage {
               'NB':140,
               'DIR':'go straight',
             },
-          'relatedBeacons':[140]
+          'relatedBeacons':[139,140]
         },
       {
         'beaconID':140,
@@ -83,6 +84,33 @@ export class BeaconPage {
           "DIR":'turn left'
         }],
         'relatedBeacons':[146,153,158,156]
+      },
+      {
+        'beaconID':153,
+        'beaconInfo': {
+          'PB':158,
+          'NB':'',
+          'DIR':'go straight'
+        },
+        'relatedBeacons':[153,158,159]
+      },
+      {
+        'beaconID':156,
+        'beaconInfo': {
+          'PB':158,
+          'NB':'',
+          'DIR':'go straight'
+        },
+        'relatedBeacons':[156,158]
+      },
+      {
+        'beaconID':159,
+        'beaconInfo': {
+          'PB':153,
+          'NB':'',
+          'DIR':'go straight'
+        },
+        'relatedBeacons':[153,159]
       }
     ]
   }
@@ -173,6 +201,9 @@ export class BeaconPage {
               if(JSON.stringify(this.currentBeacon)==JSON.stringify(this.shortestPath[pathCounter])){
                 this.nextBeaconToGo=this.shortestPath[pathCounter+1];
               }
+              else if(JSON.stringify(this.currentBeacon)==JSON.stringify(this.shortestPath[this.shortestPath.length-1])){
+                this.nextBeaconToGo="already at destination";
+              }
             }
             let alert = this.alertCtrl.create({
               title:'Directie',
@@ -183,14 +214,58 @@ export class BeaconPage {
             console.log("Nearest beacon: " + this.pBeaconAccuracy + "beaconId: "+ this.currentBeacon);
             this.isFirstScan=false;
             this.previousBeacon=this.currentBeacon;
-            console.log(this.previousBeacon);
-            console.log(this.shortestPath);
           }
           else{
-            console.log("second scan");
+            if (data.beacons.length > 0) {
+              
+                for(let i=0;i<this.beaconDetails.length;i++){
+                  if(this.beaconDetails[i]["beaconID"]==this.previousBeacon){
+                    this.testForRelated=data.beacons;
+                  // this.testForRelated.splice(1,1);
+                    console.log(this.testForRelated);
+                    console.log("before splice");
+                    for(let j=0;j<this.testForRelated.length;j++){
+                      if(this.beaconDetails[i]["relatedBeacons"].includes(parseInt(this.testForRelated[j]["major"]))==false){
+                        this.testForRelated.splice(j,1);
+                        console.log(this.testForRelated);
+                        console.log("after splice");
+                      }
+                    }
+                  }
+                }
+                for(let i=0;i<this.testForRelated.length;i++){
+                  if(i==0){    
+                    this.pBeaconAccuracy=this.testForRelated[i]["accuracy"]
+                    this.currentBeacon=this.testForRelated[i]["major"];
+                  }
+                  else{
+                    if(this.pBeaconAccuracy>this.testForRelated[i]["accuracy"]){
+                      this.pBeaconAccuracy=this.testForRelated[i]["accuracy"];
+                      this.currentBeacon=this.testForRelated[i]["major"];
+                    }
+                  }
+                }//end of for loop
+                if(this.nextBeaconToGo==this.currentBeacon){
+                  for(let pathCounter=0;pathCounter<this.shortestPath.length;pathCounter++){
+                    if(JSON.stringify(this.currentBeacon)==JSON.stringify(this.shortestPath[pathCounter])){
+                      this.nextBeaconToGo=this.shortestPath[pathCounter+1];
+                    }
+                    else if(JSON.stringify(this.currentBeacon)==JSON.stringify(this.shortestPath[this.shortestPath.length-1])){
+                      this.nextBeaconToGo="already at destination";
+                    }
+                  }
+                  this.previousBeacon=this.currentBeacon;
+                  let alert = this.alertCtrl.create({
+                    title:'Directie',
+                    subTitle: 'current beacon: '+this.currentBeacon+'<br> Next Beacon: '+this.nextBeaconToGo,
+                    buttons:['OK']
+                  });
+                  alert.present();
+                }
+               
+               
+            }
           }
-          
-          
       },
         error => console.error()
       );
@@ -212,68 +287,26 @@ export class BeaconPage {
   }
   
   inputDijkstra(){
-   
-    
      const Graph = require('node-dijkstra');
      const route = new Graph();
-    //let obj={ B:1 };
-   /* route.addNode('139', {140:1});
-    route.addNode('140', {139:1,146:1});
-    route.addNode('146', { 140:1, 158:1});
-    route.addNode('158', { 146:1});
-    //route.addNode('D', { C:1, B:4 });
-    
-    this.shortestPath=route.path('140', '158');
-
-    console.log(this.shortestPath);
-    
-    */
-    /*
-    */
         for(let i=0;i<this.beaconDetails.length;i++){
           this.relatedBeacon={};
           if(this.beaconDetails[i]["relatedBeacons"].length>=1){
             for(let j=0;j<this.beaconDetails[i]["relatedBeacons"].length;j++){
               if(j==0){
                 this.relatedBeacon[this.beaconDetails[i]["relatedBeacons"][j]]=1;
-                //this.relatedBeacon="{"+this.beaconDetails[i]["relatedBeacons"][j]+":1";
               }
               else{
                 this.relatedBeacon[this.beaconDetails[i]["relatedBeacons"][j]]=1;
-                //this.relatedBeacon=this.relatedBeacon+", "+this.beaconDetails[i]["relatedBeacons"][j]+":1";
               }
             }
           }
           route.addNode(JSON.stringify(this.beaconDetails[i]["beaconID"]),this.relatedBeacon);
-          let obj2=this.relatedBeacon;
-          //let obj3={151:1,152:1};
-          //console.log(obj);
-          //console.log(obj3);
           console.dir("beaconID:"+this.beaconDetails[i]["beaconID"]+"related: "+(this.relatedBeacon));
-          console.log(obj2);
-          
-          //console.log(this.relationTest);
-          //console.log(this.beaconDetails[i]["relatedBeacons"][0]);
-          //console.log(this.beaconDetails[i]["beaconID"]+","+obj2)
         }
         
-        this.shortestPath=route.path('139', '156');
-          console.log(route.path('139', '156'));
-          /* const graph = new Map()
-      
-      const a = new Map()
-      a.set('B', 1)
-      
-      const b = new Map()
-      b.set('A', 1)
-      b.set('C', 2)
-      b.set('D', 4)
-      
-      graph.set('A', a)
-      graph.set('B', b);
-      
-      const route = new Graph(graph)
-      */
+        this.shortestPath=route.path('139', '159');
+          console.log(route.path('139', '159'));
   }
  
 }
