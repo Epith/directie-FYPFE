9125f50c78cf95e710b1c5e75144fcdfb031076b
Foott <ng_hong_yao@hotmail.com>
2018-07-11 16:58:28 +0800

    Created api for compass
    -

--- Directie/src/pages/compass-bearing/compass-bearing.ts
+++ Directie/src/pages/compass-bearing/compass-bearing.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation';
-
+import { ApiProvider } from '../../providers/api/api';
 
 /**
  * Generated class for the CompassBearingPage page.
@@ -17,8 +17,9 @@ import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-nativ
 })
 export class CompassBearingPage {
   private bearing:any;
-  private beaconRelation:any;
-  private beaconDetails:any;
+  //private beaconRelation:any;
+  //private beaconDetails:any;
+  private beacon:any;
   private beaconList:any=[];
   selectedPreviousBeacon:any;
   selectedCurrentBeacon:any;
@@ -28,91 +29,97 @@ export class CompassBearingPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     private deviceOrientation: DeviceOrientation,
+    public apiProvider: ApiProvider,
     private alertCtrl: AlertController) {
-    this.beaconRelation={
-      "Beacons":[
+    this.getBeacon();
+    /*this.beaconRelation = {
+      "Beacons": [
         {
-          'beaconID':139,
-          'beaconInfo':[
+          'beaconID': 139,
+          'beaconInfo': [
             {
-              'PB':'',
-              'NB':140,
-              'DIR':'Go straight',
-              'bearing':330
+              'PB': null,
+              'NB': 140,
+              'DIR': 'Go straight',
+              'Bearing': 200
             }
-          ], 
-          'relatedBeacons':[139,140],
-          'turningPoint':false
-        },
-      {
-        'beaconID':140,
-        'beaconInfo':[ 
-          {
-            'PB':139,
-            'NB':146,
-            'DIR':'Go Straight'
-          }],
-        'relatedBeacons':[139,140,146],
-        'turningPoint':false
-      },
-      {
-        'beaconID':146,
-        'beaconInfo': [{
-          'PB':140,
-          'NB':158,
-          'DIR':'Turn Left'
-        }],
-        'relatedBeacons':[140,146,158],
-        'turningPoint':true
-      },
-      {
-        'beaconID':158,
-        'beaconInfo': [{
-          'PB':146,
-          'NB':153,
-          'DIR':'Go Straight'
+          ],
+          'relatedBeacons': [139, 140],
+          'turningPoint': false
         },
         {
-          'PB':146,
-          'NB':156,
-          "DIR":'go right'
-        }],
-        'relatedBeacons':[146,153,158,156],
-        'turningPoint':false
-      },
-      {
-        'beaconID':153,
-        'beaconInfo': [{
-          'PB':158,
-          'NB':'159',
-          'DIR':'Go straight'
-        }],
-        'relatedBeacons':[153,158,159],
-        'turningPoint':false
-      },
-      {
-        'beaconID':156,
-        'beaconInfo': {
-          'PB':158,
-          'NB':'',
-          'DIR':'Go straight'
+          'beaconID': 140,
+          'beaconInfo': [
+            {
+              'PB': 139,
+              'NB': 146,
+              'DIR': 'Go Straight',
+              'Bearing': 250
+            }],
+          'relatedBeacons': [139, 140, 146],
+          'turningPoint': false
         },
-        'relatedBeacons':[156,158],
-        'turningPoint':false
-      },
-      {
-        'beaconID':159,
-        'beaconInfo': {
-          'PB':153,
-          'NB':'',
-          'DIR':'Go straight'
+        {
+          'beaconID': 146,
+          'beaconInfo': [{
+            'PB': 140,
+            'NB': 158,
+            'DIR': 'Turn Left',
+            'Bearing': 290
+          }],
+          'relatedBeacons': [140, 146, 158],
+          'turningPoint': true
         },
-        'relatedBeacons':[153,159],
-        'turningPoint':false
-      }
-    ]
-  }
-    this.beaconDetails=this.beaconRelation["Beacons"];
+        {
+          'beaconID': 158,
+          'beaconInfo': [{
+            'PB': 146,
+            'NB': 153,
+            'DIR': 'Go Straight',
+            'Bearing': 200
+          },
+          {
+            'PB': 146,
+            'NB': 156,
+            "DIR": 'go right'
+          }],
+          'relatedBeacons': [146, 153, 158, 156],
+          'turningPoint': false
+        },
+        {
+          'beaconID': 153,
+          'beaconInfo': [{
+            'PB': 158,
+            'NB': 159,
+            'DIR': 'Go straight',
+            'Bearing': 200
+          }],
+          'relatedBeacons': [153, 158, 159],
+          'turningPoint': false
+        },
+        {
+          'beaconID': 156,
+          'beaconInfo': {
+            'PB': 158,
+            'NB': null,
+            'DIR': 'Go straight'
+          },
+          'relatedBeacons': [156, 158],
+          'turningPoint': false
+        },
+        {
+          'beaconID': 159,
+          'beaconInfo': {
+            'PB': 153,
+            'NB': null,
+            'DIR': 'Go straight'
+          },
+          'relatedBeacons': [153, 159],
+          'turningPoint': false
+        }
+      ]
+    }*/
+    //this.beaconDetails=this.beaconRelation["Beacons"];
         // Get the device current compass heading
     this.deviceOrientation.getCurrentHeading().then(
       (data: DeviceOrientationCompassHeading) => console.log(data),
@@ -124,19 +131,31 @@ export class CompassBearingPage {
       (data: DeviceOrientationCompassHeading) => this.bearing=Math.round(data.magneticHeading)
     );
 
-    this.loadBeaconsIntoList();
-    console.log(this.beaconList);
+    //this.loadBeaconsIntoList();
+    //console.log(this.beaconList);
   }
-
+  getBeacon(){
+    let data = {
+    }
+    this.apiProvider.getBeacon(data)
+    .then(data => {
+      this.beacon = data;
+      for(let i=0;i<this.beacon.length;i++){
+        this.beaconList.push(this.beacon[i]["BeaconID"]);
+      }
+      console.log(this.beaconList);
+    });
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad CompassBearingPage');
   }
 
-  loadBeaconsIntoList(){
+  /*loadBeaconsIntoList(){
     for(let i=0;i<this.beaconDetails.length;i++){
       this.beaconList.push(this.beaconDetails[i]["beaconID"]);
     }
-  }
+  }*/
 
   callConsole(){
     console.log(this.selectedPreviousBeacon);
@@ -146,6 +165,20 @@ export class CompassBearingPage {
   }
 
   sendToDatabase(){
+
+    var CurrentBID = parseInt(this.selectedCurrentBeacon);
+    var PreviousBID = parseInt(this.selectedPreviousBeacon);
+    var NextBID = parseInt(this.selectedNextBeacon);
+    var DirectID;
+    if (this.directionToGo == "Go Sraight"){
+      DirectID = 1;
+    }else if (this.directionToGo == "Turn Left"){
+      DirectID = 2;
+    }else if (this.directionToGo == "Turn Right"){
+      DirectID = 3;
+    }
+    var Compass = this.bearing;
+
     let alert = this.alertCtrl.create({
       title: 'Confirm Data',
       message: 'Send to the database?',
@@ -161,6 +194,24 @@ export class CompassBearingPage {
           text: 'Confirm',
           handler: () => {
             console.log('Buy clicked');
+            
+
+            var data = {
+              "BeaconDirection" : {
+                "CurrentBID":CurrentBID,
+                "PreviousBID":PreviousBID,
+                "NextBID":NextBID,
+                "DirectID":DirectID,
+                "Compass":Compass
+              }
+            }
+            console.log(data);
+            this.apiProvider.createBeaconDirection(data).then((result) => {
+              console.log(result);
+            }, (err) => {
+              console.log(err);
+            });
+            
           }
         }
       ]
